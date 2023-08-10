675a6b636a236a3be3457d9296432c7bade02769
Alexander <a.ayugov@zebrains.team>
2023-08-10 17:19:52 +0400

    layout card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import style from './Card.module.scss'
 
 function Card() {
   return (
-    <div>Card</div>
+    <div className={style.card}>
+      <div className={style.body}>
+        <p className={style.text}>Tap here</p>
+      </div>
+      <div className={style.buttons}>
+        <button className={style.button}>#8EF664</button>
+        <button className={style.button}>Закрыть</button>
+      </div>
+    </div>
   )
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Card from '../Card/Card'
 
 function Main() {
   return (
     <main className='main'>
       <div className="main__container container">
-        контент
-        
+        <h1 className='title'>Карточки</h1>
+        <Card/>
       </div>
     </main>
   )
